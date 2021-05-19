6ac3079438305faf8f4de4db024142b2deecdc4e
Thoo <40025225+thoo0224@users.noreply.github.com>
2021-05-20 01:30:37 +0200

    improved docs

--- src/EFCore.GenericRepository.Core/IRepository.cs
+++ src/EFCore.GenericRepository.Core/IRepository.cs
@@ -1,5 +1,9 @@
 ﻿namespace EFCore.GenericRepository.Core
 {
+    /// <summary>
+    /// Repository
+    /// </summary>
+    /// <typeparam name="TEntity">The entity for the Repository.</typeparam>
     public interface IRepository<TEntity>
         where TEntity : class
     {
--- src/EFCore.GenericRepository.Core/IRepositoryFactory.cs
+++ src/EFCore.GenericRepository.Core/IRepositoryFactory.cs
@@ -3,10 +3,10 @@
 namespace EFCore.GenericRepository.Core
 {
     /// <summary>
-    /// Factory for the Repository.
+    /// Factory for the Repository. This will create the repository using services from the service collection.
     /// </summary>
-    /// <typeparam name="TEntity">Entity of the Repository</typeparam>
-    /// <typeparam name="TRepo">Repository</typeparam>
+    /// <typeparam name="TEntity">Entity of the <see cref="IRepository{TEntity}"/></typeparam>
+    /// <typeparam name="TRepo">Type for <see cref="IRepository{TEntity}"/>\</typeparam>
     public interface IRepositoryFactory<TEntity, out TRepo>
         where TEntity : class
         where TRepo : IRepository<TEntity>
@@ -15,9 +15,9 @@ namespace EFCore.GenericRepository.Core
         /// <summary>
         /// Creates the repository
         /// </summary>
-        /// <param name="dbContext">If there's already a db context, we can use that for the new repository.</param>
-        /// <returns>Repository</returns>
-        public TRepo Create(DbContext dbContext = null);
+        /// <param name="existingDbContext">An existing <see cref="DbContext"/> so we don't have to create a new one.</param>
+        /// <returns>The <see cref="IRepository{TEntity}"/> that has been created.</returns>
+        public TRepo Create(DbContext existingDbContext = null);
 
     }
 }
